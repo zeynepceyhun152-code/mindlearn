2b5ec98e824ed5fb5270dcb68669b1522cec78fe
Zeynep Ceyhun <zeynep.ceyhun.152@gmail.com>
2026-06-01 18:13:57 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    // This forces production builds to successfully complete even i
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // This allows production builds to successfully complete even if
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
